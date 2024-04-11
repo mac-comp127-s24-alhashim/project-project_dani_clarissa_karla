7203b1fbf73c615711a865d0da8101a8960c0b4a
ClarissaSigale <csigale@macalester.edu>
2024-04-11 10:46:26 -0500

    Skeleton code

---
+++ src/Displays.java
@@ -0,0 +1,32 @@
+public class Displays {
+    
+    private void changeBody(){
+
+    }
+
+    private void changeEars(){
+        
+    }
+
+    private void changeTail(){
+        
+    }
+
+    private void changeHands(){
+        
+    }
+
+    private void changeFeet(){
+        
+    }
+
+    private void changeEyes(){
+        
+    }
+
+    private void changeAcc(){
+        
+    }
+
+
+}
--- src/MainGame.java
+++ src/MainGame.java
@@ -1,11 +1,35 @@
+import java.util.List;
+
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.FontStyle;
 import edu.macalester.graphics.GraphicsText;
+import edu.macalester.graphics.Image;
+import edu.macalester.graphics.ui.Button;
 
 public class MainGame {
 
-    public String creatureName;
+    private String creatureName;
     public CanvasWindow canvas;
+
+    private List<String> bodyList = List.of("");
+    private List<String> eyesList = List.of("");
+    private List<String> mouthList = List.of("");
+    private List<String> accessoriesList = List.of("");
+    private List<String> handsList = List.of("");
+    private List<String> feetList = List.of("");
+    private List<String> tailList = List.of("");
+
+
+
+    private Image body = new Image(bodyList.get(0));
+    private Image eyes = new Image(eyesList.get(0));
+    private Image mouth = new Image(mouthList.get(0));
+    private Image accessories = new Image(accessoriesList.get(0));
+    private Image hands = new Image(handsList.get(0));
+    private Image feet = new Image(feetList.get(0));
+    private Image tail = new Image(tailList.get(0));
+    
+
 
     public MainGame(){
         canvas = new CanvasWindow("Main Window", 1000, 750);
@@ -13,37 +37,14 @@ public class MainGame {
         startText.setFont("Times New Roman", FontStyle.PLAIN, 50);
         startText.setCenter(500, 100);
         canvas.add(startText);
-    }
-
-    private void changeBody(){
-
-    }
-
-    private void changeEars(){
-        
-    }
-
-    private void changeTail(){
-        
-    }
-
-    private void changeHands(){
-        
-    }
-
-    private void changeFeet(){
-        
-    }
-
-    private void changeEyes(){
-        
-    }
-
-    private void changeAcc(){
-        
+        Button startButton = new Button("Start");
+        startButton.setScale(5);
+        startButton.setCenter(500, 500);
+        canvas.add(startButton);
     }
 
     public static void main(String[] args) {
         MainGame mainGame = new MainGame();
     }
+    
 }
