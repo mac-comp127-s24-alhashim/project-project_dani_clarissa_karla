f2efced57cf44dcdd8506b3bf2a56859ebb1b481
ClarissaSigale <csigale@macalester.edu>
2024-04-15 11:08:20 -0500

    Update Button

--- src/MainGame.java
+++ src/MainGame.java
@@ -5,8 +5,10 @@ import edu.macalester.graphics.FontStyle;
 import edu.macalester.graphics.GraphicsText;
 import edu.macalester.graphics.Image;
 import edu.macalester.graphics.Rectangle;
-import edu.macalester.graphics.ui.Button;
+// import edu.macalester.graphics.ui.Button;
+import SizableButton.*;
 import java.awt.Color;
+
 
 public class MainGame {
 
@@ -32,6 +34,12 @@ public class MainGame {
     private Image tail = new Image(tailList.get(0));
     
 
+    // Button jb = new Button("Test", 200, 200);
+    // jb.setPosition(100, 100);
+    // jb.onClick(() -> jb.setPosition(jb.getX() + 2, jb.getY() + 2));
+
+    // window.add(jb);
+
 
     public MainGame(){
         canvas = new CanvasWindow("Main Window", 1000, 750);
@@ -39,11 +47,10 @@ public class MainGame {
         startText.setFont("Times New Roman", FontStyle.PLAIN, 50);
         startText.setCenter(500, 100);
         canvas.add(startText);
-        Button startButton = new Button("Start");
-        startButton.setScale(5);
+        Button startButton = new Button("Start", 100, 40);
         startButton.setCenter(500, 500);
         canvas.add(startButton);
-        Rectangle buttonBack = new Rectangle(0, 0, 80, 40);
+        Rectangle buttonBack = new Rectangle(0, 0, 115, 55);
         buttonBack.setFillColor(Color.PINK);
         buttonBack.setCenter(500, 500);
         canvas.add(buttonBack);
