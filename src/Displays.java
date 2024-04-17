49ef225463fe108ca52fdd3c12446cce88b17288
karlamartinez03 <kaitaely@gmail.com>
2024-04-17 11:01:00 -0500

    eyes

--- src/Displays.java
+++ src/Displays.java
@@ -1,6 +1,22 @@
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Image;
+import javax.imageio.ImageIO;
+import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.GraphicsObject;
+import edu.macalester.graphics.GraphicsText;
+import edu.macalester.graphics.Point;
+
 public class Displays {
+    private Image currentBody;
+    private double maxWidth = Double.POSITIVE_INFINITY, maxHeight = Double.POSITIVE_INFINITY;
+
     
-    private void changeBody(){
+    private void changeBody(CanvasWindow canvas, String imagePath){
+        currentBody = new Image(0,0);
+        if(currentBody != null){
+            
+        }
+
 
     }
 
@@ -28,5 +44,12 @@ public class Displays {
         
     }
 
-
+    /**
+     * Causes the image to shrink (preserving aspect ratio) if the image width is larger than the
+     * given maximum width.
+     */
+    // public void setMaxWidth(double maxWidth) {
+    //     this.maxWidth = maxWidth;
+    //     changed();
+    // }
 }
