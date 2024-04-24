3769fe90cdcc49e4fb183919f1437e52e95bbac0
daniarana <156988538+daniarana@users.noreply.github.com>
2024-04-24 11:45:49 -0500

    updated HUD class

--- src/Hud.java
+++ src/Hud.java
@@ -12,10 +12,72 @@ import java.awt.Color;
  * Creates a HUD window with a specific number of buttons
  */
 public class Hud {
-    public Hud(String type, int num){
+    public Hud(String type, int num, MainGame mainGame){
+        CanvasWindow canvas = mainGame.canvas;
         if (type == "Color"){
+            Rectangle outerbox = new Rectangle(50, 90, 275, 150);
+            outerbox.setFillColor(new Color(255, 111, 0));
+            outerbox.setStroked(true);
+            canvas.add(outerbox);
 
-            //max 7
+            GraphicsText boxText1 = new GraphicsText("Colors");
+            boxText1.setFont("Times New Roman", FontStyle.PLAIN, 25);
+            boxText1.setCenter(175,125);
+            canvas.add(boxText1);
+
+            Button color1 = new Button("", 40, 40);
+            color1.setPosition(70, 150);
+            color1.setFillColor(new Color(140, 158, 255));
+            //color1.setLineColor(new Color(140, 158, 255));
+            canvas.add(color1);
+            
+        color1.onClick(() -> {
+            
+        });
+            
+        Button color2 = new Button("", 40, 40);
+            color2.setFillColor(Color.RED);
+            // color2.setLineColor(Color.RED);
+            color2.setPosition(150, 150);
+            canvas.add(color2);
+            
+        // color2.onClick(() -> {
+        //     ;
+        // });
+
+        Button color3 = new Button("green", 40, 40);
+            color3.setPosition(230, 150);
+            color3.setFillColor(new Color(140, 158, 255));
+            //color1.setLineColor(new Color(140, 158, 255));
+            canvas.add(color3);
+            
+        // color3.onClick(() -> {
+        //     ;
+        // });
+
+        Button color4 = new Button("pink", 40, 40);
+            color4.setPosition(110, 190);
+            color4.setFillColor(new Color(140, 158, 255));
+            //color1.setLineColor(new Color(140, 158, 255));
+            canvas.add(color4);
+            
+        // color4.onClick(() -> {
+        //     ;
+        // });
+
+        Button color5 = new Button("purple", 40, 40);
+            color5.setPosition(190, 190);
+            color5.setFillColor(new Color(140, 158, 255));
+            //color1.setLineColor(new Color(140, 158, 255));
+            canvas.add(color5);
+            
+        // color5.onClick(() -> {
+        //     ;
+        // });
+
+        }
+        else if (type == ""){
+
         }
 
 
